bd349137feeddbd027ded414df7f1960c821fe98
Yuvraj@162002 <yuvrajpareek44@gmail.com>
2021-08-26 18:12:06 +0530

    commit

--- app/src/main/java/com/example/android/inputconstraints/Constants.java
+++ app/src/main/java/com/example/android/inputconstraints/Constants.java
@@ -1,6 +1,7 @@
 package com.example.android.inputconstraints;
 
 public class Constants {
+    // regex..
     public static final String REGEX = "regex";
     public static final String INPUT_DATA = "input data";
 }
